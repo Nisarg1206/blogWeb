790d9ae9a02146b262f2239a4db162dfdb60d649
Nisarg <nisargpatel1206@gmail.com>
2025-01-07 08:40:12 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Toaster } from 'react-hot-toast'
+import { configureStore } from '@reduxjs/toolkit';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <React.StrictMode>
+        <App></App>
+        {/* <BrowserRouter> */}
+            
+            {/* <Toaster></Toaster> */}
+        {/* </BrowserRouter> */}
+    </React.StrictMode>
+);
